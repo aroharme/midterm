ba39699a211194d375ce06ba686e6d56bd6303c7
arora <arora@LAPTOP-ID0C6C53>
2025-02-20 18:17:15 -0500

    Initialized the magicHand array and checked for a user input card

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -1,39 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetic;
 
 import java.util.Scanner;
 
-/** This class takes String input plus,minus,divide and times
- * from user and execute the arithmetic operation
- * change the code to use enum instead String and mention the advantage of enum.
- * @author sivagamasrinivasan
- * 
- */
-public class ArithmeticBase 
-{
- public double x,y;
-    double calculate(double x, double y) 
-        {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
-                return x + y;
-            case "MINUS":
-                return x - y;
-            case "TIMES":
-                return x * y;
-            case "DIVIDE":
-                return x / y;
-            default:
-                throw new AssertionError("Unknown operations " + this);
-        }
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        // Create an instance of ArithmeticBase
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
+        
+        // Get user input for the two numbers
+        System.out.println("Enter first number: ");
+        double n = in.nextDouble();
+        
+        System.out.println("Enter second number: ");
+        double m = in.nextDouble();
+        
+        // Get user input for the operation
+        System.out.println("Enter arithmetic operation to perform (PLUS, MINUS, TIMES, DIVIDE): ");
+        String operationInput = in.next().toUpperCase();
+        
+        // Parse the input into an enum value
+        ArithmeticOperation operation = ArithmeticOperation.valueOf(operationInput);
+
+        // Calculate the result using the selected operation
+        double result = r.calculate(m, n, operation);
+        
+        // Print the result
+        System.out.println("Result: " + result);
     }
-   
 }
---
+++ src/arithmetic/ArithmeticOperation.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package arithmetic;
+
+/**
+ *
+ * @author arora
+ */
+public enum ArithmeticOperation {
+    
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE;
+}
+    
+}
